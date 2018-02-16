1aafe302b1d7bcad4bb4da45fe25e49104619e63
Uriah Carpenter <uriah@widen.com>
2018-02-16 15:12:54 -0600

    typos

--- .buildkite/pipeline-docker.sh
+++ .buildkite/pipeline-docker.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-set -euox pipefail
+set -euo pipefail
 
 export DOCKER_TAG=$(echo "${BUILDKITE_BRANCH}-${BUILDKITE_COMMIT:0:8}" | tr '[:upper:]' '[:lower:]' | sed 's/\//-/g');
-exprot THIS_BK_AGENT=$(buildkite-agent meta-data get name)
-cat pipline-docker.yml
+export THIS_BK_AGENT=$(buildkite-agent meta-data get name)
+cat .buildkite/pipeline-docker.yml
