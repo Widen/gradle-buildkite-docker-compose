7bc7551200f545ca5138610e8df7627dc0f0e6b8
Uriah Carpenter <uriah@widen.com>
2018-02-19 17:53:38 -0600

    do not echo in pipeline script

--- .buildkite/test.sh
+++ .buildkite/test.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+echo "--- DOCKER_TAG is '$DOCKER_TAG'"
+
 export GRADLE_OPTS="-Dorg.gradle.daemon=false"
 export TERM="dumb"
 ./gradlew clean test --continue
