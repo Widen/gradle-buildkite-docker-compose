b681ace170800327248dc6357e4af59032c37c34
Uriah Carpenter <uriah@widen.com>
2018-02-19 18:08:51 -0600

    env testing

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-export DOCKER_TAG=$(echo "${BUILDKITE_BRANCH}-${BUILDKITE_COMMIT:0:8}" | tr '[:upper:]' '[:lower:]' | sed 's/\//-/g')
+DOCKER_TAG=$(echo "${BUILDKITE_BRANCH}-${BUILDKITE_COMMIT:0:8}" | tr '[:upper:]' '[:lower:]' | sed 's/\//-/g')
+#buildkite-agent meta-data set build-docker-tag $DOCKER_TAG
+
 cat .buildkite/pipeline.yml
--- .buildkite/test.sh
+++ .buildkite/test.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-echo "--- DOCKER_TAG is '$DOCKER_TAG'"
-
 export GRADLE_OPTS="-Dorg.gradle.daemon=false"
 export TERM="dumb"
 ./gradlew clean test --continue
